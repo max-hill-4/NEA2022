52177d741938528795d0a34dd1a25c89805615bc
Max <maxwellhill2004@icloud.com>
2022-02-12 01:24:01 +0000

    commit

--- config.py
+++ config.py
@@ -10,16 +10,16 @@ loginScreenColor=(255,80,150)
 
 
 # Pygame creates  hidden surface ready to be shown on screen.
-button_login_image = py.image.load("recources/button_login_image.png")
-button_create_image = py.image.load("recources/button_create_image.png")
-button_quit_image = py.image.load("recources/button_quit_image.png")
-button_back_image = py.image.load("recources/button_back_image.png")
-button_confirm_image = py.image.load("recources/button_confirm_image.png")
+button_login_image = py.image.load("resources/button_login_image.png")
+button_create_image = py.image.load("resources/button_create_image.png")
+button_quit_image = py.image.load("resources/button_quit_image.png")
+button_back_image = py.image.load("resources/button_back_image.png")
+button_confirm_image = py.image.load("resources/button_confirm_image.png")
 
-text_box_image =  py.image.load("recources/text_box_image.png")
-text_box_image_selected = py.image.load("recources/text_box_image_selected.png")
+text_box_image =  py.image.load("resources/text_box_image.png")
+text_box_image_selected = py.image.load("resources/text_box_image_selected.png")
 
-title_name_image = py.image.load("recources/title_name_image.png")
+title_name_image = py.image.load("resources/title_name_image.png")
 
 
 
