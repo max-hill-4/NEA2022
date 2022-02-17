a51b8b66dc3daea42a5026054c29e22081695590
Max <maxwellhill2004@icloud.com>
2022-02-17 01:35:19 +0000

    database update

--- database.py
+++ database.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 59533fcc7056d16176b9db2907a0435f441330ac
 import mysql.connector
 from mysql.connector import errorcode
 
@@ -18,53 +14,6 @@ mycursor = mydb.cursor()
 
 
 mycursor.execute("SELECT * FROM accounts")
-
-myresult = mycursor.fetchall()
-
-for x in myresult:
-  print(x)
-<<<<<<< HEAD
-#test
-=======
-#test
-=======
-import mysql.connector
-from mysql.connector import errorcode
-
-
-mydb = mysql.connector.connect(
-        host='database-1.c1ajdf2akg8w.eu-west-2.rds.amazonaws.com',
-        user='admin', 
-        password = "eTWgAS3ZvDzSpA2",
-        database = "test"
-        )
-
-
-mycursor = mydb.cursor()
-
-
-mycursor.execute("SELECT * FROM accounts")
-
-myresult = mycursor.fetchall()
-
-for x in myresult:
-  print(x)
->>>>>>> origin/main
->>>>>>> 59533fcc7056d16176b9db2907a0435f441330ac
-import mysql.connector
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="yourusername",
-  password="yourpassword",
-  database="mydatabase"
-)
-
-mycursor = mydb.cursor()
-
-sql = "SELECT * FROM customers WHERE address ='Park Lane 38'"
-
-mycursor.execute(sql)
 
 myresult = mycursor.fetchall()
 
